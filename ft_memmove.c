aa5961d4b1e4d3bae46e40423ee8041e68eda150
kanh296 <kieuanhhoang2906@gmail.com>
2022-10-27 00:50:44 +0300

    memcpy test, strlcat

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nghoang <nghoang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 17:10:52 by nghoang           #+#    #+#             */
-/*   Updated: 2022/10/25 17:10:52 by nghoang          ###   ########.fr       */
+/*   Updated: 2022/10/26 23:05:36 by nghoang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	if (dst == NULL)
-		return (NULL);
-	
+	unsigned char	tmp[n];
+
+	ft_memcpy(tmp, src, n);
+	ft_memcpy(dst, tmp, n);
+	return (dst);
 }
+
